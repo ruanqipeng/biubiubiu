86ff219d234af982473c115c515f3f95e40f8de0
ruanqipeng <32729885+ruanqipeng@users.noreply.github.com>
2017-10-12 15:51:42 +0800

    Update WelcomContr.java

--- WelcomContr.java
+++ WelcomContr.java
@@ -5,25 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//åœ¨æˆ‘å†™è¿™æ®µä»£ç çš„æ—¶å€™ åªæœ‰æˆ‘å’Œä¸Šå¸çŸ¥é“å®ƒæ˜¯å¹²ä»€ä¹ˆçš„
+//ç°åœ¨åªæœ‰ä¸Šå¸çŸ¥é“
 @Controller
 @RequestMapping
 public class WelcomContr {
    	
    @RequestMapping
    public String weluuu(Model model){
-	   model.addAttribute("msg", "Í¨¹ı×¢½âµÃµ½µÄ¿ØÖÆÆ÷£¡");
+	   model.addAttribute("msg", "é€šè¿‡æ³¨è§£å¾—åˆ°çš„æ§åˆ¶å™¨ï¼");
 	   return "index";
    }
    
    
    @RequestMapping(value="/welcom1")	
    public String weluu1(Model model){
-	   model.addAttribute("msg", "Í¨¹ı×¢½âµÃµ½µÄ¿ØÖÆÆ÷1£¡");
+	   model.addAttribute("msg", "é€šè¿‡æ³¨è§£å¾—åˆ°çš„æ§åˆ¶å™¨1ï¼");
 	   return "user/login";
    }
    
-   //¸ñÊ½£ºspringmvc001/action2/1/3
+   //æ ¼å¼ï¼šspringmvc001/action2/1/3
    @RequestMapping("/action2/{p1}/{p2}")	
    public String wel2(Model model,@PathVariable int p1,@PathVariable int p2){
 	   model.addAttribute("msg", "p1:"+p1+"---p2:"+p2);
@@ -40,20 +41,20 @@ public class WelcomContr {
    
    @RequestMapping(value="/action4?/**/*")	
    public String wel4(Model model){
-	   model.addAttribute("msg", "ant·ç¸ñ");
+	   model.addAttribute("msg", "anté£æ ¼");
 	   return "index";
    }
    
-   //Î½´Ê£ºÇëÇó·½Ê½
+   //è°“è¯ï¼šè¯·æ±‚æ–¹å¼
    @RequestMapping(value="/action5",method={RequestMethod.GET})	
    public String wel5(Model model){
-	   model.addAttribute("msg", "ant·ç¸ñ");
+	   model.addAttribute("msg", "anté£æ ¼");
 	   return "index";
    }
    
    @RequestMapping(path={"/myqction","/action6"})	
    public String wel6(Model model){
-	   model.addAttribute("msg", "ant·ç¸ñ");
+	   model.addAttribute("msg", "anté£æ ¼");
 	   return "index";
    }
    
